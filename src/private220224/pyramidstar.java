cfe9f25d702545861c3bd88a2e944d0053e7c87e
Munyoulim <99157613+Munyoulim@users.noreply.github.com>
2022-02-24 17:07:37 +0900

    220224_pm

---
+++ src/private220224/pyramidstar.java
@@ -0,0 +1,37 @@
+package private220224;
+
+public class pyramidstar {
+	public static void main(String[] args) {
+		String pyramid = " ";
+		
+		for (int i = 0; i < 7; i++) {
+			for (int j = 6; j > i; j--) {
+				pyramid += " ";
+				
+			}
+			for (int k = 0; k < 2 * i - 1; k++) {
+				pyramid += "*";
+				
+			}
+			pyramid += "\n";
+			
+		}
+		for(int i = 0; i < 9; i++){
+		    for(int k = -1; k < i ; k++){
+		        pyramid += " ";
+		    	
+		    }
+		    for(int j = 8; j > 2 * i - 1; j--){
+		        pyramid += "*";
+		    	
+		    }
+		    
+		    pyramid += "\n";
+		    
+		} System.out.println(pyramid);
+	}
+}
+
+
+
+
