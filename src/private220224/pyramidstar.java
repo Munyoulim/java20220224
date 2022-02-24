feab35201b996af3daeb6900ce3f4bc02e59a92a
Munyoulim <99157613+Munyoulim@users.noreply.github.com>
2022-02-24 17:23:50 +0900

    220224_pm

--- src/private220224/pyramidstar.java
+++ src/private220224/pyramidstar.java
@@ -9,18 +9,22 @@ public class pyramidstar {
 				pyramid += " ";
 				
 			}
+			
 			for (int k = 0; k < 2 * i - 1; k++) {
 				pyramid += "*";
 				
 			}
+			
 			pyramid += "\n";
 			
 		}
+		
 		for(int i = 0; i < 9; i++){
 		    for(int k = -1; k < i ; k++){
 		        pyramid += " ";
 		    	
 		    }
+		    
 		    for(int j = 8; j > 2 * i - 1; j--){
 		        pyramid += "*";
 		    	
