11cd847edad6b50a195c9f3665fddcb4ee28c44d
Munyoulim <99157613+Munyoulim@users.noreply.github.com>
2022-02-24 12:12:34 +0900

    220224_am

---
+++ src/chap01/Hello.java
@@ -0,0 +1,17 @@
+package chap01;
+
+/* 
+ * 작성자 : 문유림
+ * 작성일 : 2022.02.24
+ */
+
+public class Hello {
+	// 프로그램 실행 진입점
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, welcome to the java world!");
+		
+		// 단축키 : ctrl + /
+		
+	} // end of main
+} // end of class
