698fb44e172e9b53f437e1714b245933e70e9ddd
Munyoulim <99157613+Munyoulim@users.noreply.github.com>
2022-02-28 00:32:16 +0900

    chap03_exercise

---
+++ src/chap03/bookk/C03DenyLogicOperatorExample.java
@@ -0,0 +1,14 @@
+package chap03.bookk;
+
+public class C03DenyLogicOperatorExample {
+	public static void main(String[] args) {
+		boolean play = true;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+	}
+}
