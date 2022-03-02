d0370d78c6ee16dff596b6ac81c2be5d59bc9a4b
Munyoulim <99157613+Munyoulim@users.noreply.github.com>
2022-03-02 23:12:44 +0900

    220302

--- src/private220224/Star.java
+++ src/private220224/Star.java
@@ -2,21 +2,23 @@ package private220224;
 
 public class Star {
 	public static void main(String[] args) {
-		String pyramid = " ";
-		
-		for (int i = 0; i < 7; i++) {
-			for (int j = 6; j > i; j--) {
-				pyramid += " ";
-				
-			}
-			
-			for (int k = 0; k < 2 * i - 1; k++) {
-				pyramid += "*";
-				
-			}
-			
-			pyramid += "\n";
-			
+		for (int i = 1; i <= 9; i++) {
+
+		    // 공백 j for문
+		    for (int j = 9; j > i; j--) {
+		        System.out.print(" ");
+		    }
+		    // 별 j for문
+		    for (int j = 1; j <= 2 * i - 1; j++) {
+		        System.out.print("*");
+		    }
+		    // 공백 j for문
+		    for (int j = 9; j > i; j--) {
+		        System.out.print(" ");
+		    }
+		    
+		    System.out.println("");
+		    
 		}
 	}
 }
