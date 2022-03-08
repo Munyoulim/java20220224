4d8fdb3054c4550d23e964215c51440920637e71
Munyoulim <99157613+Munyoulim@users.noreply.github.com>
2022-03-08 21:47:59 +0900

    220308

--- src/chap05/book/exercise/Exercise09.java
+++ src/chap05/book/exercise/Exercise09.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Exercise09 {
 	public static void main(String[] args) {
+		
 		boolean run = true;
 		int studentNum = 0;
 		int[] scores = null;
@@ -11,9 +12,9 @@ public class Exercise09 {
 		Scanner scanner = new Scanner(System.in);
 		
 		while(run) {
-			System.out.println("-------------------------------");
-			System.out.println("1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료");
-			System.out.println("-------------------------------");
+			System.out.println("--------------------------------");
+			System.out.println("1. 학생수 | 2. 점수입력 | 3. 점수리스트 | 4. 분석 | 5. 종료");
+			System.out.println("--------------------------------");
 			System.out.println("선택>");
 			
 			int selectNo = scanner.nextInt();
@@ -22,7 +23,33 @@ public class Exercise09 {
 				System.out.print("학생수>");
 				studentNum = scanner.nextInt();
 				scores = new int[studentNum];
+				
+			} else if (selectNo == 2) {
+				for (int i = 0; i < studentNum; i++) {
+					System.out.print("scores[" + i + "] : ");
+					int score = scanner.nextInt();
+					scores[i] = score;
+				}
+			} else if (selectNo == 3) {
+				for (int i = 0; i < studentNum; i++) {
+					System.out.println("scores[" + i + "] : " + scores[i]);
+				}
+			} else if (selectNo == 4) {
+				int max = 0;
+				int sum = 0;
+				double avg = 0;
+				for (int i = 0; i < studentNum; i++) {
+					sum += scores[i];
+					max = (max < scores[i]) ? scores[i] : max;
+				}
+				avg = (double) sum / studentNum;
+				System.out.println("최고 점수 : " + max);
+				System.out.println("평균 점수 : " + avg);
+			} else if (selectNo == 5) {
+				run = false;
 			}
 		}
+		System.out.println("프로그램 종료");
+		//scanner.close();
 	}
 }
