59f394b13f7e902cdd0eb64d9af6a26884645826
Munyoulim <99157613+Munyoulim@users.noreply.github.com>
2022-03-08 12:30:55 +0900

    220308

---
+++ src/chap05/lecture/string/C02String.java
@@ -0,0 +1,19 @@
+package chap05.lecture.string;
+
+public class C02String {
+	public static void main(String[] args) {
+		
+	
+	String s1 = new String("java");
+	String s2 = new String("hello");
+	
+	String s3 = "java";
+	String s4 = "hello";
+	
+	System.out.println(s1.length());
+	System.out.println(s2.length());
+	System.out.println(s3.length());
+	System.out.println(s4.length());
+	
+	}
+}
