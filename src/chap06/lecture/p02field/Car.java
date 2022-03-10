ee8779b0c663ae3c04ecace548adafd861bd9039
Munyoulim <99157613+Munyoulim@users.noreply.github.com>
2022-03-10 18:34:28 +0900

    220310

--- src/chap06/lecture/p02field/Car.java
+++ src/chap06/lecture/p02field/Car.java
@@ -2,4 +2,6 @@ package chap06.lecture.p02field;
 
 public class Car {
 	int speed; // field
+	String color;
+	String model;
 }
---
+++ src/chap06/lecture/p02field/Desk.java
@@ -0,0 +1,7 @@
+package chap06.lecture.p02field;
+
+public class Desk {
+	int height = 120;
+	int weight = 100;
+	String color = "green";
+}
