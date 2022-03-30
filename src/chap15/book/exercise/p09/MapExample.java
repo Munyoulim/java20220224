2c0a744b50e38c5de424156e1208adf1756bc15f
Munyoulim <99157613+Munyoulim@users.noreply.github.com>
2022-03-30 17:33:33 +0900

    220330

---
+++ src/chap15/book/exercise/p09/MapExample.java
@@ -0,0 +1,19 @@
+package chap15.book.exercise.p09;
+
+import java.util.*;
+
+public class MapExample {
+	public static void main(String[] args) {
+		Map<String, Integer> map = new HashMap<>();
+		
+		map.put("blue", 96);
+		map.put("hong", 86);
+		map.put("white", 92);
+		
+		String name = null; // 최고 점수를 받은 아이디 저장
+		int maxScore= 0; // 최고 점수 저장
+		int totalScore = 0; // 점수 합계 저장
+		
+		
+	}
+}
