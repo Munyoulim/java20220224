dda596cf9ed5311ff0556b40e124f94ef43dffdd
Munyoulim <99157613+Munyoulim@users.noreply.github.com>
2022-03-31 23:46:20 +0900

    220331

--- src/chap15/book/exercise/p09/MapExample.java
+++ src/chap15/book/exercise/p09/MapExample.java
@@ -14,6 +14,34 @@ public class MapExample {
 		int maxScore= 0; // 최고 점수 저장
 		int totalScore = 0; // 점수 합계 저장
 		
+		// 모두 탐색
+		// 1. entrySet
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			System.out.println(entry);
+		}
 		
+		// 2. keySet
+		Set<String> keySet = map.keySet();
+		for (String key : keySet) {
+			System.out.println(key + "=" + map.get(key));
+		}
+		
+		// 3. forEach
+		map.forEach((k, v) -> System.out.println(k + "=" + v));
+		
+		// 확인문제 9
+		keySet = map.keySet();
+		for (String key : keySet) {
+			Integer value = map.get(key);
+			
+			if (value > maxScore) {
+				name = key;
+				maxScore = value;
+			}
+			totalScore += value;
+		}
+		System.out.println("평균점수 : " + (totalScore / (double) map.size()));
+		System.out.println("최고점수 : " + maxScore);
+		System.out.println("최고아이디 : " + name);
 	}
 }
