921055b6b045bd2ea50a85a6fad24f07bb37b1bc
Munyoulim <99157613+Munyoulim@users.noreply.github.com>
2022-04-01 14:03:29 +0900

    220401

---
+++ src/Private/nothing220401.java
@@ -0,0 +1,13 @@
+package Private;
+
+public class nothing220401 {
+	public static void main(String[] args) {
+		int[] arr1 = new int[] {3, 1, 5, 6, 8};
+		
+		for (int i = 0; i < arr1.length; i++) {
+			System.out.println(arr1[i]);
+		}
+		
+		
+	}
+}
